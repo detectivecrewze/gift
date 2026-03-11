d2550d0f1bcf57b0422a3ab1ad232c9e8dc2aff4
detectivecrewze <aldoramadhan16@gmail.com>
2026-03-12 00:20:14 +0700

    chore: add default config.js template to arcade folder

---
+++ arcade/config.js
@@ -0,0 +1,32 @@
+/**
+ * Premium Standalone Configuration Template
+ * 
+ * Untuk Paket PREMIUM:
+ * Ganti isi file ini dengan data yang Anda terima dari Telegram Admin.
+ */
+
+window.STANDALONE_CONFIG = {
+  "id": "premium-template",
+  "recipient_name": "Luna",
+  "message": "You are the center of my universe. ✦",
+  "date": "2026-03-21",
+  "password": null,
+  "audio": {
+    "url": "https://pub-8419616ae90b4353846669935ed798ba.r2.dev/1710156000-sample.mp3",
+    "name": "Floating in Space",
+    "artist": "The Astronauts"
+  },
+  "photos": [
+    {
+      "url": "https://pub-8419616ae90b4353846669935ed798ba.r2.dev/1710156000-sample.jpg",
+      "caption": "Where it all started."
+    }
+  ],
+  "active_apps": {
+    "journey": true,
+    "moments": true,
+    "quiz": true,
+    "bucket_list": true,
+    "message": true
+  }
+};
